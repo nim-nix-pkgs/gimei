d1477af5e4b6213dcf62ca9bd51fbb1ea9216742
hugosenari <hugosenari@gmail.com>
2022-05-09 20:37:43 -0300

    chore: re index gimei

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."gimei-master".dir   = "master";
-  inputs."gimei-master".owner = "nim-nix-pkgs";
-  inputs."gimei-master".ref   = "master";
-  inputs."gimei-master".repo  = "gimei";
-  inputs."gimei-master".type  = "github";
-  inputs."gimei-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."gimei-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
